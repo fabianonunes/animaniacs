55dde56af85ce34a7d6613e55514ca336a2e223e
Fabiano Parente <fabiano@nunes.me>
2019-05-06 10:17:19 -0300

    remove arquivos js

--- app/animaniacs.js
+++
@@ -1,6 +0,0 @@
-const { readFileSync } = require('fs')
-const { resolve } = require('path')
-
-exports.yakko = readFileSync(resolve(__dirname, './art/YAKKO'))
-exports.wakko = readFileSync(resolve(__dirname, './art/WAKKO'))
-exports.dot = readFileSync(resolve(__dirname, './art/DOT'))

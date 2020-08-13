bdeb23322cd39dc31fd05df8ace3eb6546ed27ad
Fabiano Parente <fabiano@nunes.me>
2020-08-13 13:09:28 -0300

    buffered

--- animaniacs.go
+++ animaniacs.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/DataDog/zstd"
 	"github.com/Pallinder/go-randomdata"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/memstore"
@@ -30,7 +32,23 @@ type ZstdParams struct {
 	Threads int `form:"T"`
 }
 
+var netClient = &http.Client{
+	Timeout: time.Second * 600,
+}
+
 var f = fmt.Sprintf
+
+func compress(input io.Reader, output io.Writer, params *ZstdParams) *exec.Cmd {
+	cmd := exec.Command(
+		"zstd",
+		"-qc",
+		f("-T%d", params.Threads),
+		f("-%d", params.Level),
+	)
+	cmd.Stdin = input
+	cmd.Stdout = output
+	return cmd
+}
 
 func main() {
 	char := os.Getenv("CHAR")
@@ -39,8 +57,60 @@ func main() {
 	store := memstore.NewStore([]byte("secret"))
 	r.Use(sessions.Sessions("sessionid", store))
 
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Success")
+
+	globalBuffer := new(bytes.Buffer)
+	defaultParams := &ZstdParams{
+		Level:   3,
+		Threads: 0,
+	}
+
+	r.GET("/zec-pipe", func(context *gin.Context) {
+		if globalBuffer.Len() > 0 {
+			reader := bytes.NewReader(globalBuffer.Bytes())
+			context.DataFromReader(
+				http.StatusOK, int64(globalBuffer.Len()), "application/zec", reader, nil,
+			)
+			return
+		}
+
+		globalBuffer.Truncate(0)
+		_ = context.ShouldBindQuery(&defaultParams)
+
+		responseWriter := context.Writer
+		zecPipeReader, zecPipeWriter := io.Pipe()
+		zstdPipeReader, zstdPipeWriter := io.Pipe()
+		localBuffer := new(bytes.Buffer)
+		tee := io.TeeReader(zstdPipeReader, localBuffer)
+
+		defer func() {
+			_ = zecPipeWriter.Close()
+			_ = zstdPipeWriter.Close()
+		}()
+
+		go func() {
+			response, _ := netClient.Get("http://localhost:8080/apps/iterator")
+			contentLength := response.Header.Get("Content-Length")
+			context.Header("sf.com.zec-length", contentLength)
+			defer func() {
+				_ = response.Body.Close()
+				_ = zecPipeWriter.Close()
+			}()
+			_, _ = io.Copy(zecPipeWriter, response.Body)
+		}()
+
+		cmd := compress(zecPipeReader, zstdPipeWriter, defaultParams)
+
+		go func() {
+			if _, err := io.Copy(responseWriter, tee); err != nil {
+				_ = zstdPipeWriter.Close()
+			}
+		}()
+
+		if err := cmd.Run(); err != nil {
+			localBuffer.Reset()
+		} else {
+			globalBuffer = localBuffer
+		}
 	})
 
 	r.GET("/zec-compress", func(context *gin.Context) {
@@ -48,8 +118,8 @@ func main() {
 		fileName := os.Getenv("ZEC_PATH")
 
 		params := &ZstdParams{
-			Level:   1,
-			Threads: 2,
+			Level:   2,
+			Threads: 1,
 		}
 		_ = context.ShouldBindQuery(&params)
 
@@ -67,6 +137,7 @@ func main() {
 
 		go func() {
 			if _, err := io.Copy(writer, pipeReader); err != nil {
+				// TODO: testar se um defer pipeWriter.Close() nesse closure tem o mesmo efeito
 				_ = pipeWriter.Close()
 			}
 		}()
@@ -74,9 +145,32 @@ func main() {
 		_ = cmd.Run()
 	})
 
+	r.GET("/zec-compress-go", func(context *gin.Context) {
+		params := &ZstdParams{
+			Level: 2,
+		}
+		_ = context.ShouldBindQuery(&params)
+
+		writer := context.Writer
+		filePath := os.Getenv("ZEC_PATH")
+
+		file, _ := os.Open(filePath)
+
+		zstdWriter := zstd.NewWriterLevel(writer, params.Level)
+		defer zstdWriter.Close()
+
+		if _, err := io.Copy(zstdWriter, file); err != nil {
+			zstdWriter.Close()
+		}
+	})
+
 	r.GET("/zec", func(context *gin.Context) {
 		filePath := os.Getenv("ZEC_PATH")
 		context.File(filePath)
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Success")
 	})
 
 	r.GET("/count", func(c *gin.Context) {
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module fabianonunes/animaniacs
 go 1.13
 
 require (
+	github.com/DataDog/zstd v1.4.5
 	github.com/Pallinder/go-randomdata v1.2.0
 	github.com/gin-contrib/sessions v0.0.3
 	github.com/gin-gonic/gin v1.6.3
 )
+
+//replace github.com/gin-gonic/gin => github.com/fabianonunes/gin v1.6.3-chunked
